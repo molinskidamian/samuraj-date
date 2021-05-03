33bae543330dfe5baf38545fa5e4b4810b643a0b
Damian Molinski <tedeer@gmail.com>
2021-05-03 13:57:20 +0200

    Clock in the middle side

---
+++ main.js
@@ -0,0 +1,33 @@
+span = document.querySelector('span');
+span.classList.add('red');
+
+const addZero = (item) => {
+  if(item < 10) {
+    return item.textContent = `0${item}`;
+  }
+  return item;
+}
+
+
+const clock = () => {
+    let time = new Date();
+
+    // Przykładowe metody które są dostępne:
+    console.log(`time.getFullYear(): ${time.getFullYear()}`);
+    console.log(`time.toLocaleString(): ${time.toLocaleString()}`);
+    console.log(`time.toLocaleDateString(): ${time.toLocaleDateString()}`);
+    console.log(`time.getTime(): ${time.getTime()}`);
+
+    console.log("-------------------");
+
+    const seconds = time.getSeconds();
+    const minutes = time.getMinutes();
+    const hours = time.getHours();
+
+    let myFormat = `${addZero(hours)} : ${addZero(minutes)} : ${addZero(seconds)}`;
+    console.log(myFormat);
+
+    span.textContent = myFormat;
+}
+
+setInterval(clock, 1000);
